7358d7d4f62c33056f1e6d14950d7a7cbb0bfe55
amir199675 <amirderakhshanfar251@gmail.com>
2019-08-26 19:50:32 +0430

    two_after_latest

--- salon_yab/settings.py
+++ salon_yab/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd#j=xug=#l0qy5$lybh@%uk8a(as2)@3-f4pz9sqfddcwu*tox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
 DEBUG = False
 
 ALLOWED_HOSTS = ['www.salon-yab.ir','salon-yab.ir','http://salon-yab.ir/','http://salon-yab.ir/','130.185.76.161']
