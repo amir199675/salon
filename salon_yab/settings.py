c7bddbf232bdf581f06f3edc2b06f4ce910d895b
amir199675 <amirderakhshanfar251@gmail.com>
2019-10-15 10:32:47 +0330

    test_dargah

--- salon_yab/settings.py
+++ salon_yab/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd#j=xug=#l0qy5$lybh@%uk8a(as2)@3-f4pz9sqfddcwu*tox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
-#
-# ALLOWED_HOSTS = ['www.salon-yab.ir','salon-yab.ir','https://salon-yab.ir/','https://salon-yab.ir/','130.185.76.161']
-ALLOWED_HOSTS = []
+DEBUG = False
+# DEBUG = True
+
+ALLOWED_HOSTS = ['www.salon-yab.ir','salon-yab.ir','https://salon-yab.ir/','https://salon-yab.ir/','130.185.76.161']
+# ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = '../../dashboard/'
 
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'salon_yab.wsgi.application'
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql',
-		'NAME': "pushsalonyab",
-		# 'NAME': "salonyab",
+		# 'NAME': "pushsalonyab",
+		'NAME': "salonyab",
 		'PASSWORD': 'j+AXw.79',
-		'USER': 'postgres',
-		# 'USER': 'amir',
+		# 'USER': 'postgres',
+		'USER': 'amir',
 
 		'PORT': 5432
 	}
